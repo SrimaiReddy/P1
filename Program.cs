e02b76946d8254f320e095afd37dec75dfce5aa2
SrimaiReddy <S531384@nwmissouri.edu>
2019-01-23 22:00:19 -0600

    Added some functionality

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User!");
+//             Declare and use a bool, int, double, char, string, and decimal type.
+// Implement at least one loop
+// Implement at least one if or if-else branch with conditions
+// Implement a switch statement
+// Implement a simple function calculation (e.g. totalCost or hasEnough)
+// Use Console.WriteLine and Console.ReadLine
+// Use string interpolation ($"") and \n or \t
+        }
+    }
+}
